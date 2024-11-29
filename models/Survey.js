ab9e30db8b75086f100e3429ec399b8e30ba4930
amazaing16 <zain.garuba@ontariotechu.net>
2024-11-28 23:29:41 -0500

    Updated survey model

--- models/Survey.js
+++ models/Survey.js
@@ -1,15 +1,12 @@
-// models/Survey.js
 const mongoose = require('mongoose');
 
-// Define the Survey schema
 const surveySchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String },
     status: { type: String, enum: ['Active', 'Draft', 'Published', 'Closed'], default: 'Active' },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Link to the user who created the survey
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 }, { timestamps: true });
 
-// Create the Survey model
 const Survey = mongoose.model('Survey', surveySchema);
 
 module.exports = Survey;
